7edf31a079827fbe6bcc520142979c9d7540b774
Arindam Bose <arindam.1993@gmail.com>
2024-06-26 19:52:36 -0700

    remove hardcoded bbox

--- src/generateVectorTiles.js
+++ src/generateVectorTiles.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const geojson2mvt = require('geojson2mvt');
+const bbox = require('@turf/bbox').default;
 
 import('gtfs-to-geojson').then((gtfsToGeoJSON) => {
   const agencies = [
@@ -10,37 +11,34 @@ import('gtfs-to-geojson').then((gtfsToGeoJSON) => {
       path: path.join(__dirname, '../gtfs/GTFSTransitData_RG.zip'),
     }
   ];
-
+  
+  return gtfsToGeoJSON.default({
+    agencies,
+    outputType: 'agency',
+    outputFormat: 'lines-and-stops',
+    ignoreDuplicates: true,
+  });
+}).then(() => {
+  console.log('geojson generated');
   // This is a hardcoded temp geojson file thats an intermediary
   const outputGeojsonPath = path.join(process.cwd(), '/geojson/RG/RG.geojson');
   
+  const geojson = JSON.parse(fs.readFileSync(outputGeojsonPath, {encoding: 'utf-8'}));
+  const boundingBox = bbox(geojson);
   const options = {
     layerName: 'layer0',
     rootDir: './tiles',
     bbox: [
-      36.61398006570329,
-      -123.83095440232262,
-      39.21353032569155,
-      -120.83959624268564,
+      boundingBox[1],
+      boundingBox[0],
+      boundingBox[3],
+      boundingBox[2],
     ], //[south,west,north,east]
     zoom: {
       min: 7,
       max: 15,
     }
   };
-  
-  
-  return gtfsToGeoJSON({
-    agencies,
-    outputType: 'agency',
-    outputFormat: 'lines-and-stops',
-    ignoreDuplicates: true,
-  });
-  
-}).then(() => {
-  console.log('geojson generated');
-  const geojson = JSON.parse(fs.readFileSync(outputGeojsonPath, {encoding: 'utf-8'}));
-  debugger;
 
   // /tiles output director gets generated at cwd
   geojson2mvt(geojson, options);
