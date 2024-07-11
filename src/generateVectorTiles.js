e3d59cc2e94bfdefbb4f8e8583dc7e89e60524e0
Arindam Bose <arindam.1993@gmail.com>
2024-07-10 20:40:52 -0700

    refactor to use await

--- src/generateVectorTiles.js
+++ src/generateVectorTiles.js
@@ -2,28 +2,64 @@ const path = require('path');
 const fs = require('fs');
 const geojson2mvt = require('geojson2mvt');
 const bbox = require('@turf/bbox').default;
+const turf = require('@turf/helpers');
 
-import('gtfs-to-geojson').then((gtfsToGeoJSON) => {
-  const agencies = [
-    {
-      agency_key: 'RG',
-      // Input GTFS file path
-      path: path.join(__dirname, '../gtfs/GTFSTransitData_RG.zip'),
+const agencies = [
+  {
+    agency_key: 'RG',
+    // Input GTFS file path
+    path: path.join(__dirname, '../gtfs/GTFSTransitData_RG.zip'),
+  }
+];
+
+
+function flattenStopRoutes(geojson) {
+  const stopIdPointDict = {};
+  const routeIdLineStringDict = {};
+
+  for(const feature of geojson.features){
+    if(feature.geometry.type === 'Point'){
+      const routes = feature.properties.routes;
+      feature.properties['route_ids'] = routes.map((route) => route.route_id).join(',');
+      delete feature.properties.routes;
+
+      const stopId = feature.properties['stop_id'];
+      if (stopId && stopId.length > 0 ) {
+        const point = turf.point(feature.geometry.coordinates);
+        stopIdPointDict[stopId] = point
+      }
+    } else if(feature.geometry.type === 'LineString') {
+      const routeId = feature.properties.route_id;
+      if (routeId && routeId.length > 0) {
+        const lineString = turf.lineString(feature.geometry.coordinates);
+        routeIdLineStringDict[routeId] = lineString;
+      }
     }
-  ];
+  }
+
+  return {stopIdPointDict, routeIdLineStringDict};
+}
+
+const lineClippingDict = {};
+
+(async () => {
+  const gtfsToGeoJSON = await import('gtfs-to-geojson');
   
-  return gtfsToGeoJSON.default({
+  await gtfsToGeoJSON.default({
     agencies,
     outputType: 'agency',
     outputFormat: 'lines-and-stops',
     ignoreDuplicates: true,
   });
-}).then(() => {
+
   console.log('geojson generated');
+
   // This is a hardcoded temp geojson file thats an intermediary
   const outputGeojsonPath = path.join(process.cwd(), '/geojson/RG/RG.geojson');
   
   const geojson = JSON.parse(fs.readFileSync(outputGeojsonPath, {encoding: 'utf-8'}));
+  const {stopIdPointDict, routeIdLineStringDict} = flattenStopRoutes(geojson);
+
   const boundingBox = bbox(geojson);
   const options = {
     layerName: 'layer0',
@@ -42,4 +78,4 @@ import('gtfs-to-geojson').then((gtfsToGeoJSON) => {
 
   // /tiles output director gets generated at cwd
   geojson2mvt(geojson, options);
-});
+})();
