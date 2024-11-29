b4dcf2c84ebda15ce0d84071d23e5753cd217b37
Arindam Bose <arindam.1993@gmail.com>
2024-11-28 22:16:47 -0800

    Delete useless thing

--- src/generate-route-line-clipping-lookup-tables.js
+++ src/generate-route-line-clipping-lookup-tables.js
@@ -1,13 +1,10 @@
-const { createReadStream } = require('node:fs');
 const { writeFile } = require('node:fs/promises');
 const { getRouteTripShapeLookup } = require('./get-route-id-trip-id-shape-id-lookup');
 const { getShapesLookup } = require('./get-shapes-lookup');
-const { getDistanceAlongLookup } = require('./distance-along-lookup-helper');
 const { resolve } = require("path");
-const { parse } = require('csv-parse');
 
 /**
- * Computes three lookup tables:
+ * Computes 2 lookup tables:
  * 1. stopTripShapeLookup: 
  *    This is a two-level dictionary
  *    Level1 :
@@ -16,14 +13,8 @@ const { parse } = require('csv-parse');
  *       Key is a trip-id, Value is a shape-id
  * 2. shapeIdLineStringLookup:
  *    Key is the shape-id of a route, and the value is a LineString of the entire route
- * 3. distanceAlongLookup:
- *    This is a two-level dictionary
- *    Level1 :
- *    Key is a stop-id, Value is the 2nd Level dictionary
- *       Level 2:
- *       Key is a trip-id, Value is how far along the LineString the stop is for that trip.
  *
- * These three lookup tables frovide enough information to generate a LineString for a
+ * These two lookup tables frovide enough information to generate a LineString for a
  * trip thats clipped between the entry and exit stops. 
  *
  * @param {string} unzippedGtfsPath path to unzipped gtfs text files
@@ -40,18 +31,10 @@ async function generateRouteLineClippingLookupTables(
 
   const shapeIdLineStringLookup = await getShapesLookup(unzippedGtfsPath);
   console.log('Built <shape-id> : <LineString> table');
-
-  const stopTimesReadableStream = createReadStream(resolve(unzippedGtfsPath, `stop_times.txt`), {encoding: 'utf8'});
-  const parser = stopTimesReadableStream.pipe(parse({columns: true}));
-
-  // Lookup tables for clipping LineStrings
-  const distanceAlongLookup = await getDistanceAlongLookup(parser);
-  console.log('Built <route-id, trip-id> : <distance-along> table');
   
   const routlineLookups = {
     stopTripShapeLookup,
     shapeIdLineStringLookup,
-    distanceAlongLookup,
   };
 
   await writeFile(
@@ -59,8 +42,6 @@ async function generateRouteLineClippingLookupTables(
     JSON.stringify(routlineLookups),
     'utf8',
   );
-
-  stopTimesReadableStream.close();
 };
 
 module.exports = {
